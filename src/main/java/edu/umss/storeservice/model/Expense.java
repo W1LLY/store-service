5ba25333030a41d0822f6983078ec15a7fb63666
Willy Sanchez Rojas <willy.sanchez@dharbor.com>
2019-07-02 22:56:04 -0400

    reinitialized files

---
+++ src/main/java/edu/umss/storeservice/model/Expense.java
@@ -0,0 +1,52 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package edu.umss.storeservice.model;
+
+import edu.umss.storeservice.dto.ExpenseDto;
+
+import javax.persistence.*;
+
+@Entity
+public class Expense extends ModelBase<ExpenseDto> {
+    @OneToMany
+    private ExpenseType expenseType;
+    private Long value;
+    private String description;
+
+    @ManyToOne
+    private ItemInstance itemInstance;
+
+    public ExpenseType getExpenseType() {
+        return expenseType;
+    }
+
+    public void setExpenseType(ExpenseType expenseType) {
+        this.expenseType = expenseType;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ItemInstance getItemInstance() {
+        return itemInstance;
+    }
+
+    public void setItemInstance(ItemInstance itemInstance) {
+        this.itemInstance = itemInstance;
+    }
+}
---
+++ src/main/java/edu/umss/storeservice/model/ExpenseType.java
@@ -0,0 +1,23 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package edu.umss.storeservice.model;
+
+import edu.umss.storeservice.dto.ExpenseTypeDto;
+
+import javax.persistence.Entity;
+
+@Entity
+public class ExpenseType extends ModelBase<ExpenseTypeDto>{
+
+    private String typeName;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+}
